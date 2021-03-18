3dd17da004bc9f4594d612de767276cfaef8ba0a
Adam Janiš <adam.janis@gmail.com>
2021-03-18 16:53:53 +0100

    add chat widget

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,9 @@ export default {
    */
   head: {
     title: 'Statusflare - Monitoring Made Easy',
+    script: [
+      { src: '/tawk.js', body: true },
+    ],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
